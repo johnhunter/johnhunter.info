067204b0e1940f021fc14263f75a241196640fe1
John Hunter <john@johnhunter.info>
2022-04-23 17:15:21 +0100

    Improve test setup and utils

--- utils/setupTestFixture.js
+++ utils/setupTestFixture.js
@@ -1,41 +1,77 @@
 const fs = require('fs');
 const path = require('path');
+const { JSDOM } = require('jsdom');
+const rootDir = process.cwd();
+const { dir } = require(path.resolve(rootDir, '.eleventy.js'));
 
-const rootDir = path.resolve(__dirname, '../');
-
-const getPageHtml = filePath => {
+const getPageHtml = (filePath) => {
   try {
     const html = fs.readFileSync(path.resolve(rootDir, filePath), 'utf8');
     return html.toString();
-  } catch(err) {
-    throw(new Error(`Cannot find page '${filePath}'. Have you built the site?`));
+  } catch (err) {
+    throw new Error(`Cannot find page '${filePath}'. Have you built the site?`);
   }
 };
 
+const createDomfromHtml = (html, options) =>
+  new JSDOM(html, options).window.document;
+
+const copyElementAttrs = (source, target) => {
+  Array.from(source.attributes || []).forEach(({ name, value }) => {
+    target.setAttribute(name, value);
+  });
+};
+
 /**
- * Boilerplate to load a page into the test document.
- *
+ * Boilerplate to load a page from the site output into the test document.
  * Is available as a global within tests.
  *
- * @param {string} filePath path to the file under test (relative to rootDir)
+ * Note: <script> tags are not evaluated
+ *
+ * @param {string} filePath path to the file under test
+ * @returns {{
+ *  query: function,
+ *  queryAll: function,
+ *  getAttributes: function,
+ *  getTitle: function,
+ *  getMeta: function,
+ *  getLinkHref: function
+ * }}
  */
 const loadPage = (filePath) => {
   if (!document || !document.documentElement) {
-    throw(new Error('loadPage requires `document` to be defined'));
+    throw new Error('loadPage requires `document` to be defined');
   }
 
-  document.documentElement.innerHTML = getPageHtml(filePath);
+  const htmlStr = getPageHtml(path.resolve(dir.output, filePath));
+  document.documentElement.innerHTML = htmlStr;
 
-  const query = sel => document.querySelector(sel);
-  const queryAll = sel => Array.from(document.querySelectorAll(sel));
+  const dom = createDomfromHtml(htmlStr);
+  copyElementAttrs(dom.documentElement, document.documentElement);
+
+  const query = (sel) => document.querySelector(sel);
+  const queryAll = (sel) => Array.from(document.querySelectorAll(sel));
+  const getAttrs = (el) => {
+    if (!el.hasAttributes()) {
+      return {};
+    }
+
+    return Array.from(el.attributes).reduce((acc, attr) => {
+      if (attr.name) {
+        acc[attr.name] = attr.value;
+      }
+      return acc;
+    }, {});
+  };
 
   return {
     query,
     queryAll,
+    getAttrs,
     getTitle: () => query('title').textContent,
-    getMeta: name => query(`meta[name="${name}"]`).getAttribute('content'),
-    getStylesheets: () => queryAll('link[rel="stylesheet"]')
-      .map(node => node.getAttribute('href')),
+    getMeta: (name) => query(`meta[name="${name}"]`).getAttribute('content'),
+    getLinkHref: (rel = 'stylesheet') =>
+      queryAll(`link[rel="${rel}"]`).map((node) => node.getAttribute('href')),
   };
 };
 
